5c993280de7fc31b0756b81aba08eb70b61778f4
yu0512 <chelsea100512@gmail.com>
2020-05-29 17:54:41 +0900

    とりあえずスクレイピングテスト

--- test.py
+++ test.py
@@ -1,4 +1,22 @@
-def a():
-    print("hello, world!")
+import urllib.request
+from bs4 import BeautifulSoup
 
-a()
+url = "https://wiki.ポケモン.com/wiki/%E3%83%95%E3%82%B7%E3%82%AE%E3%83%80%E3%83%8D"
+data = None
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36"}
+
+
+# 日本語ドメインのエラー対策
+req = urllib.request.Request(url)
+req.add_header('Host', req.host.encode('idna'))
+
+# サイトのHTMLリソースを取得する
+res = urllib.request.urlopen(req)
+html = res.read()
+
+# print(html)
+
+soup = BeautifulSoup(html, "html.parser")
+tag = soup.select("#mw-content-text > div:nth-child(1) > dl:nth-child(13) > dd:nth-child(2)")
+
+print(tag.string)
